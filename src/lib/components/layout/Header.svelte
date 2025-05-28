464f70c28aa14df8304b9f567a79401417b4a8fd
bazudewa <bazudewa34@gmail.com>
2025-05-28 10:41:06 +0800

    initialize

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,9 +1,9 @@
 <script>
-  import logo from '$lib/assets/avatar.png'; // replace with your logo image
+  import logo from '$assets/avatar.png'; // replace with your logo image
 </script>
 
-<header class="max-h-80 h-20 bg-white shadow-md flex justify-center items-center">
-  <nav class="container mx-auto flex justify-between items-center">
+<header class="max-h-80 h-20 bg-white shadow-md flex justify-center items-center w-full">
+  <nav class="container mx-auto flex justify-center items-center">
     <ul class="flex justify-center items-center">
       <li class="mr-6">
         <a href="#" class="text-lg font-bold text-gray-600 hover:text-gray-900">Home</a>
@@ -12,10 +12,10 @@
         <a href="#" class="text-lg font-bold text-gray-600 hover:text-gray-900">Journey</a>
       </li>
       <li class="mx-6">
-        <img src={logo} alt="Logo" class="h-12 w-12" />
+        <img src={logo} alt="Logo" class="h-12 w-12 rounded-lg" />
       </li>
       <li class="mr-6">
-        <a href="#" class="text-lg font-bold text-gray-600 hover:text-gray-900">Project</a>
+        <a href="#" class="text-lg font-bold text-gray-600 hover:text-gray-900  hover:text-gray-900 hover:border-b-2 ">Project</a>
       </li>
       <li>
         <a href="#" class="text-lg font-bold text-gray-600 hover:text-gray-900">Contact</a>
